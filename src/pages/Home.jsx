cbe192a4e1237a678792c764ccb053f0fc2abb37
luishdalgo <lhs0003@alu.medac.es>
2025-02-10 20:12:10 +0100

    ola

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import SearchBar from '../components/SearchBar';
 function Home() {
   const [games, setGames] = useState([]);
   const [filteredGames, setFilteredGames] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     async function fetchGames() {
@@ -15,6 +16,13 @@ function Home() {
     }
     fetchGames();
   }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % games.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [games]);
 
   const handleSearch = (query) => {
     const filtered = games.filter((game) =>
@@ -28,6 +36,20 @@ function Home() {
       <div className="max-w-6xl mx-auto p-4">
         <h1 className="text-3xl font-bold mb-6 text-center text-amber-50">Juegos Populares</h1>
         
+        {/* Carrusel */}
+        <div className="relative w-full h-64 overflow-hidden mb-6 rounded-lg">
+          {games.length > 0 && (
+            
+            <img 
+              src={games[currentIndex].background_image} 
+              alt={games[currentIndex].name} 
+              className="relative w-full h-64 overflow-hidden mb-6 rounded-lg bg-gray-800"
+
+            />
+          )}
+          
+          </div>
+
         {/* Barra de búsqueda */}
         <SearchBar onSearch={handleSearch} />
         
